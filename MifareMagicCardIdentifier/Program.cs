b655cec424098e8d90832992dcfa1a653b109f23
MirakelDev <118356358+MirakelDev@users.noreply.github.com>
2024-09-06 12:57:46 +0200

    - implemented general functionality

---
+++ MifareMagicCardIdentifier/Program.cs
@@ -0,0 +1,48 @@
+﻿using LibnfcSharp;
+using LibnfcSharp.Mifare;
+using LibnfcSharp.Mifare.Extensions;
+using LibnfcSharp.Mifare.Models;
+using System;
+
+namespace MifareMagicCardIdentifier
+{
+    internal class Program
+    {
+        private static readonly byte[] DEFAULT_KEY = new byte[6] { 0x4B, 0x0B, 0x20, 0x10, 0x7C, 0xCB };
+
+        static void Main(string[] args)
+        {
+            do
+            {
+                using (var context = new NfcContext())
+                using (var device = context.OpenDevice())
+                {
+                    var mfc = new MifareClassic(device);
+                    mfc.RegisterKeyAProviderCallback((_, _) => DEFAULT_KEY);
+                    mfc.InitialDevice();
+                    mfc.WaitForCard();
+                    mfc.IdentifyMagicCardType();
+
+                    ManufacturerInfo manufacturerInfo;
+                    mfc.ReadManufacturerInfo(out manufacturerInfo);
+
+                    byte[] accessConditions;
+                    var hasUnlockedAccessConditions = mfc.HasUnlockedAccessConditions(0, out accessConditions);
+
+                    Console.WriteLine($$"""
+                        UID: {{Convert.ToHexString(mfc.Uid)}}
+                        SAK: {{Convert.ToHexString(new[] { mfc.Sak })}} ({{Convert.ToHexString(new[] { manufacturerInfo.Sak })}})
+                        ATQA: {{Convert.ToHexString(mfc.Atqa)}} ({{Convert.ToHexString(manufacturerInfo.Atqa)}})
+                        Type: {{mfc.MagicCardType.ToDescription()}}
+                        Access Conditions (Sector 0): {{Convert.ToHexString(accessConditions)}} ({{(hasUnlockedAccessConditions ? "unlocked" : "locked")}})
+
+                        Press {ENTER} to identify a new card...
+
+
+                        """);
+                }
+            }
+            while (Console.ReadKey().Key == ConsoleKey.Enter);
+        }
+    }
+}
